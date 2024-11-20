b43ddd86929ab45b07b733ce2532622e2010f9b3
SWE-645 <92914653+NimitJ99@users.noreply.github.com>
2024-11-20 18:37:36 -0500

    Initial commit

---
+++ new.js
@@ -0,0 +1,22 @@
+// const data= {
+//     name: "Nimit"
+// }
+
+// console.log(delete data); //// syntax error
+
+// const abc=[1,2,3,4]
+// console.log(delete abc[1]);
+
+// const [x,y] = abc;
+
+// console.log(x,y); // 1 2
+
+// const a = [];
+// const b = [];
+
+// console.log(a == b); //false
+// console.log(a === b); //false
+
+
+
+
